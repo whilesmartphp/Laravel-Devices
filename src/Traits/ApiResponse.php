0555cdc7e22a9fb43c51225bf97827069f317861
Kofi <kofimokome10@gmail.com>
2026-02-21 22:42:36 +0100

    enh: Add phpmd, phpstan and phpcs
    # Conflicts:
    #	composer.json

--- src/Traits/ApiResponse.php
+++ src/Traits/ApiResponse.php
@@ -11,8 +11,11 @@ trait ApiResponse
     /**
      * Return a success response.
      */
-    protected function success(mixed $data = null, string $message = 'Operation successful', int $statusCode = 200): JsonResponse
-    {
+    protected function success(
+        mixed $data = null,
+        string $message = 'Operation successful',
+        int $statusCode = 200
+    ): JsonResponse {
         return response()->json([
             'success' => true,
             'message' => $message,
@@ -23,8 +26,11 @@ trait ApiResponse
     /**
      * Return a failure response.
      */
-    protected function failure(string $message = 'Operation failed', int $statusCode = 400, array $errors = []): JsonResponse
-    {
+    protected function failure(
+        string $message = 'Operation failed',
+        int $statusCode = 400,
+        array $errors = []
+    ): JsonResponse {
         $this->error($message, $errors);
 
         return response()->json([
